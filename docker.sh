bf41ba48d808f74f30e075d56de9199c0f2221da
Joris Rijs <j.n.j.rijs@st.hanze.nl>
2020-06-30 21:52:58 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 
 sudo -i
 apt update
-ap install apt-transport-https ca-certificates curl 0software-properties-common -y
+apt install apt-transport-https ca-certificates curl 0software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt-cache policy docker-ce
