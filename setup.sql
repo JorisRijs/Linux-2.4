e635ee9edc7cb34dbbf3aed8320587f3804977c5
Joris Rijs <xps15@pop-os.localdomain>
2020-06-30 18:59:29 +0200

    changed the wordpress script arroud and added the cloud init

--- setup.sql
+++ setup.sql
@@ -1,6 +1,9 @@
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Pa$$w0rd';
 FLUSH PRIVILEGES;
 
+CREATE USER 'WordPressUser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Pa$$w0rd';
+FLUSH PRIVILEGES;
+
 CREATE DATABASE WordPressDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 GRANT ALL ON WordPressDB.* TO ' WordPressUser '@'localhost' IDENTIFIED BY 'Pa$$W0rd';
 FLUSH PRIVILEGES;
--- wordpress.sh
+++ wordpress.sh
@@ -3,22 +3,23 @@
 sudo ufw allow "apache"
 sudo ufw allow "apache Full"
 sudo ufw allow "apache Secure"
+sudo ufw enable
 
-sudo sed -i 's/index.php/index.html/' /etc/apache2.mods-enabled/dir.conf
-sudo sed -i 's/index.html/index.php/' /etc/apache2.mods-enabled/dir.conf
+sudo sed -i 's/index.php/index.html/' /etc/apache2/mods-enabled/dir.conf
+sudo sed -i 's/index.html/index.php/' /etc/apache2/mods-enabled/dir.conf
 sudo systemctl restart apache2
 
-mysql -h "localhost" -u "root" < "setup.sql"
+sudo mysql -p "Pa$$w0rd" < "$1"
 
 sudo touch /etc/apache2/sites-available/wordpress.conf
-echo "<Directory /var/www/wordpress /> AllowOverride All </Directory>"
+echo "<Directory /var/www/wordpress /> AllowOverride All </Directory>" >> /etc/apache2/sites-available/wordpress.conf
 sudo a2enmod rewrite
 sudo apache2ctl configtest
 
 cd /tmp
 sudo curl -O https://wordpress.org/latest.tar.gz
 sudo tar xzvf latest.tar.gz
-sudo touch /tmp/worpress/.htaccess
+sudo touch /tmp/wordpress/.htaccess
 sudo mv /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
 sudo mkdir /tmp/wordpress/wp-content/upgrade
 sudo cp -a /tmp/wordpress/. /var/www/wordpress
@@ -27,7 +28,7 @@ sudo chwon -R www-data:www-data /var/www/wordpress
 sudo find /var/www/wordpress/ -type d -exec chmod 750 {} \;
 sudo find /var/www/wordpress/ -type f -exec chmod 640 {} \;
 sudo curl -s https://api.wordpress.org/secret-key/1.1/salt/ >> /var/www/wordpress/wp-config.php
-sudo sed -i 's/DB_NAME/WordPressDB/' /var/www/wordpress/wp-config.php
-sudo sed -i 's/DB_USER/WordPressUSER/' /var/www/wordpress/wp-config.php
-sudo sed -i 's/DB_PASSWORD/Pa$$w0rd/' /var/www/wordpress/wp-config.php
-echo "define('FS_METHOD', 'direct')"
+sudo sed -i 's/database_name_here/WordPressDB/' /var/www/wordpress/wp-config.php
+sudo sed -i 's/username_here/WordPressUSER/' /var/www/wordpress/wp-config.php
+sudo sed -i 's/password_here/Pa$$w0rd/' /var/www/wordpress/wp-config.php
+sudo echo "define('FS_METHOD', 'direct')"
