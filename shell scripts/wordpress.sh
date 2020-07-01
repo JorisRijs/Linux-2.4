efc0ad02085fadaa651ed61971f01fd9b201891e
Joris Rijs <xps15@pop-os.localdomain>
2020-07-01 14:00:13 +0200

    more sql changes

--- shell scripts/wordpress.sh
+++ shell scripts/wordpress.sh
@@ -28,8 +28,8 @@ a2ensite wordpress
 a2enmod rewrite
 service apache2 reload
 mysql -u root -p $DBpassword -e "CREATE DATABASE wordpress;"
-mysql -u root -p $DBpassword -e "CREATE USER $DBuser@'localhost' IDENTIFIED BY $DBpassword"
-mysql -u root -p $DBpassword -e "GRANT SELECT, UPDATE, DELETE, CREATE, DROP, ALTER ON wordpress.* TO $DBuser@localhost IDENTIFIED BY $DBpassword;"
+mysql -u root -p $DBpassword -e "CREATE USER '$DBuser'@'localhost' IDENTIFIED BY '$DBpassword';"
+mysql -u root -p $DBpassword -e "GRANT SELECT, UPDATE, DELETE, CREATE, DROP, ALTER ON wordpress.* TO '$DBuser'@localhost IDENTIFIED BY '$DBpassword';"
 mysql -u root -p $DBpassword -e "FLUSH PRIVILEGES;"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
