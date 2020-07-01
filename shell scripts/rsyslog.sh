63d29e224381e903056ff581d61659442ff7a6ec
Joris Rijs <xps15@pop-os.localdomain>
2020-07-01 16:49:02 +0200

    changed the ip addresses

--- shell scripts/rsyslog.sh
+++ shell scripts/rsyslog.sh
@@ -3,9 +3,9 @@
 tee -a /etc/rsyslog.conf > /dev/null <<EOT
 
 ##Enable sending of logs over UDP add the following line:
-*.* @10.0.7.150:514
+*.* @10.0.7.199:514
 ##Enable sending of logs over TCP add the following line:
-*.* @@10.0.7.150:514
+*.* @@10.0.7.199:514
 ##Set disk queue when rsyslog server will be down:
 \$ActionQueueFileName queue
 \$ActionQueueMaxDiskSpace 1g
